628d74bec50eb334a0c1075f2da8711dc3b33831
Philip Boardman <dustykeyboard@brd.mn>
2022-08-16 08:04:26 +1000

    sst enable cors for api and s3

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,8 +1,17 @@
 import { Bucket, Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
-  const bucket = new Bucket(stack, "Uploads");
-  
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
+
   const table = new Table(stack, "Notes", {
     fields: {
       userId: "string",
